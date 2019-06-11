c6b73d662f14fb8250e75815ad7f1000e0e1137a
Pratik <pratikdhore7@gmail.com>
2019-06-11 09:48:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/feelthephoto/ShowAct.java
@@ -0,0 +1,13 @@
+package com.example.feelthephoto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
